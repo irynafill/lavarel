61effa5c2d8a73214597fff26ec3abe80dd364c3
iryna_fil <iryna_fil@epam.com>
2021-11-19 23:18:52 +0200

    add all files

---
+++ docker-compose/mysql/init_db.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS `places`;
+
+CREATE TABLE `places` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `visited` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+INSERT INTO `places` (name, visited) VALUES ('Rome',0),('Paris',0),('New York',1),('Budapest',0),('Lisbon',0),('Lisbon',0),('Porto',1),('Japan',0),('Oslo',1);
